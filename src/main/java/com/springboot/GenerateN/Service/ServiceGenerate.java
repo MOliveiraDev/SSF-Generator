05c0bd03287b7b22dc53cf05a7ac3767fd8ccaf6
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-03-09 16:55:05 -0300

    a

--- src/main/java/com/springboot/GenerateN/Service/ServiceGenerate.java
+++ src/main/java/com/springboot/GenerateN/Service/ServiceGenerate.java
@@ -2,6 +2,9 @@ package com.springboot.GenerateN.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.springboot.GenerateN.Repository.Repository;
+
+import java.util.Random;
 
 @Service
 public class ServiceGenerate {
@@ -9,5 +12,12 @@ public class ServiceGenerate {
     @Autowired
     private Repository repository;
 
-    private String gerarPrimeirosDigitos()
+    private String gerarPrimeirosDigitos(){
+        Random random = new Random();
+        StringBuilder   sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            sb.append(random.nextInt(10));
+        }
+        return sb.toString();
+    }
 }
